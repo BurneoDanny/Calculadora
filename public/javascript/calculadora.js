40636d05a05aea8b0c9001ed34a350e10aeed541
Burneo Espin Danny Jesus <djburneo@espol.edu.ec>
2023-06-28 18:54:09 -0500

    done

--- public/javascript/calculadora.js
+++ public/javascript/calculadora.js
@@ -1,26 +1,23 @@
-navBrand();
 
-function funcionPredeterminada() {
+
+var funcionPredeterminada = () => {
     const formulario = document.getElementById('formulario');
-    const valor = document.getElementById('valorTotal');
-    formulario.addEventListener('submit', (evento) => {
-        // evento.preventDefault();
-        // const horas = document.getElementById('valorPorHora');
-        // const interfaz = document.getElementById('interfazHoras');
-        // const paginas = document.getElementById('paginas');
-        // const multimedia = document.getElementById('multimedia');
-        // const mantenimiento = document.getElementById('mantenimiento');
-        // const resultado = horas + interfaz  + paginas  + multimedia  + mantenimiento;
-        // valor.value = resultado;
-
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        valorPorHora = parseInt(document.getElementById("valorPorHora").value);
+        interfaz = parseInt(document.getElementById("interfazHoras").value);
+        paginas = parseInt(document.getElementById("paginas").value);
+        hasJava = parseInt(document.getElementById("hasJavascript").value);
+        mantenimiento = parseInt(document.getElementById("mantenimiento").value);
+        result = (valorPorHora)*(interfaz+paginas+hasJava+mantenimiento);
+        document.getElementById("valorTotal").value = result;
     })
-  }
 
-function navBrand(){
-    if (window.innerWidth < 470) {
-        document.getElementsByClassName("navbar-brand").innerText = "Calculadora";    
-    }
 }
+
+funcionPredeterminada()
+
+
 
 
 
